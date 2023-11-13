7a154a379ee27a6406d1458547d57dbe89649968
Aswar12 <aswarthedoctor@gmail.com>
2023-11-13 10:52:50 +0800

    yoi

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -36,11 +36,11 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($mahasiswas as $index => $mahasiswa )
+                        @foreach ($mahasiswas as $mahasiswa )
                         <tr
                             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                             <td class="px-6 py-4">
-                                {{ $index+1 }}
+                                {{ $loop->index + 1 }}
                             </td>
                             <td class="px-6 py-4">
                                 {{ $mahasiswa->name }}
